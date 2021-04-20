687883a9649353d2159e2e556a7a4ce4492d141c
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2021-04-20 14:52:00 -0300

    Corrige margens do layout do product_item, adiciona recursos de string, cria a classe a adapter e cria função para requisição junto a API

--- app/src/main/java/com/estudo/applicationworkapi/MainActivity.kt
+++ app/src/main/java/com/estudo/applicationworkapi/MainActivity.kt
@@ -2,7 +2,14 @@ package com.estudo.applicationworkapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.estudo.applicationworkapi.api.MyRetrofit
+import com.estudo.applicationworkapi.model.Product
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +20,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         recyclerProducts = findViewById(R.id.recycler_products)
+        recyclerProducts.layoutManager = LinearLayoutManager(this)
+        getData()
+    }
+
+    private fun getData() {
+        val call: Call<List<Product>> = MyRetrofit.instance?.productApi()?.getProductApi() as Call<List<Product>>
+        call.enqueue(object : Callback<List<Product>> {
+            override fun onResponse(call: Call<List<Product>>, response: Response<List<Product>>) {
+                val adapter = ProductAdapter(this@MainActivity, response.body()?.toList() as List<Product>)
+                recyclerProducts.adapter = adapter
+            }
+
+            override fun onFailure(call: Call<List<Product>>, t: Throwable) {
+                Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_LONG).show()
+            }
+
+        })
     }
 }
